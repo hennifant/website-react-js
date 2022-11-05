ad7602a4ae67fe4a67a81649f7a31a4a14738ee6
hennifant <henning.schwichow@gmx.de>
2022-11-05 14:54:49 +0100

    Fixed extended card

--- src/data.js
+++ src/data.js
@@ -5,9 +5,11 @@ const skillsList = [
   },
   { title: "Node JS ", value: 60 },
   { title: "Express JS ", value: 70 },
-  { title: "HTML", value: 70 },
+  { title: "HTML", value: 80 },
   { title: "CSS", value: 80 },
   { title: "TypeScript", value: 50 },
+  { title: "Styled components", value: 80 },
+  { title: "Material UI", value: 70 },
 ];
 const projectList = [
   {
@@ -22,7 +24,6 @@ const projectList = [
   {
     id: 2,
     title: "Two",
-
     technologies: ["React JS", "Node JS", "MongoDB"],
     backgroundImage:
       "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
@@ -32,7 +33,6 @@ const projectList = [
   {
     id: 3,
     title: "Three",
-
     technologies: ["React JS", "Node JS", "MongoDB"],
     backgroundImage:
       "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
@@ -44,7 +44,7 @@ const projectList = [
 const experienceList = [
   {
     id: 0,
-    company: "abc",
+    company: "neue fische GmbH",
     links: {
       website: "https://www.google.com",
       facebook: "https://www.facebook.com/",

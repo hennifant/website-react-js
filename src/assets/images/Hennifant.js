32ffc0a62d1560103d1f4fe0c706c36401760581
hennifant <henning.schwichow@gmx.de>
2022-11-08 14:53:02 +0100

    Changed loader

--- src/assets/images/Hennifant.js
+++ src/assets/images/Hennifant.js
@@ -2,17 +2,92 @@ import React from "react";
 import { motion } from "framer-motion";
 import { makeStyles } from "@material-ui/core";
 
-const Hennifant = (props) => {
+const loadingCircle = {
+  display: "block",
+  width: "0.5rem",
+  height: "0.5rem",
+  backgroundColor: "black",
+  borderRadius: "0.25rem",
+};
+const loadingContainer = {
+  width: "2rem",
+  height: "2rem",
+  display: "flex",
+  justifyContent: "space-around",
+};
+
+const Hennifant = () => {
+  const loadingContainerVariants = {
+    start: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+    end: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+  const loadingCircleVariants = {
+    start: {
+      y: "0%",
+    },
+    end: {
+      y: "100%",
+    },
+  };
+  const loadingCircleTransition = {
+    duration: 0.5,
+    yoyo: Infinity,
+    ease: "easeInOut",
+  };
+
   const classes = useStyles();
   return (
-    <motion.h1>
-      <div className={classes.container}>Hennifant</div>
-    </motion.h1>
+    <div className={classes.container}>
+      <h2>Hennifant</h2>
+
+      <br />
+
+      <motion.div
+        style={loadingContainer}
+        variants={loadingContainerVariants}
+        initial="start"
+        animate="end"
+      >
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+      </motion.div>
+    </div>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    position: "fixed",
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
     backgroundColor: theme.palette.background.default,
   },
 }));
